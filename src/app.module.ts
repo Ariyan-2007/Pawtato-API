633b47674742c00b124a3a7021c75e71b9dddbb1
Safar Ahmed <safarahmed11@gmail.com>
2026-07-07 05:24:07 +0600

    Connected our first MangoBD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import configuration from './config/configuration';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './modules/database/database.module';
 import { HealthModule } from './modules/health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
