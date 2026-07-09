c0afecef34f265d3d29cbed3eed36181133f6703
Ariyan-2007 <ariyanjahangireng@gmail.com>
2026-07-09 22:12:32 +0600

    Stage Development - Phase 1

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World! Ariyan has entered the development.');
+      .expect(
+        'Hello World! Ariyan has entered the development. We shall start soon. Best Of Luck',
+      );
   });
 });
